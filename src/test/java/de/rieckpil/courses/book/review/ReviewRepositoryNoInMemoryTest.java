17d8c94701c204cee8bbb137cec12ab779e5d4ba
Jakub Vrana <vrana.jakub@feg.eu>
2023-06-05 20:21:55 +0200

    Replacing the in-memory database

--- src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryNoInMemoryTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryNoInMemoryTest.java
@@ -8,19 +8,29 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
-@Testcontainers(disabledWithoutDocker = true)
+//@Testcontainers(disabledWithoutDocker = true)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ReviewRepositoryNoInMemoryTest {
 
-  @Container
+  //  @Container
   static PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:12.3")
     .withDatabaseName("test")
     .withUsername("duke")
-    .withPassword("s3cret");
+    .withPassword("s3cret")
+    .withReuse(true); // reuse the container between tests
+
+  // The lifecycle of container is managed by @Testcontainers and @Container annotations so we don't need to start/stop it manually
+  // But we can do it if we want to by uncommenting the static block below (the singleton approach)  .. or by using @BeforeAll/@AfterAll
+  static {
+    container.start();
+  }
 
   @DynamicPropertySource
   static void properties(DynamicPropertyRegistry registry) {
@@ -29,15 +39,40 @@ class ReviewRepositoryNoInMemoryTest {
     registry.add("spring.datasource.username", container::getUsername);
   }
 
+//  @BeforeEach
+//  void beforeEach() {
+//    assertEquals(0, cut.count());
+//  }
+
   @Autowired
   private ReviewRepository cut;
 
   @Test
   @Sql(scripts = "/scripts/INIT_REVIEW_EACH_BOOK.sql")
   void shouldGetTwoReviewStatisticsWhenDatabaseContainsTwoBooksWithReview() {
+
+    List<ReviewStatistic> result = cut.getReviewStatistics();
+
+    assertEquals(3, cut.count());
+    assertEquals(2, result.size());
+
+    result.forEach(reviewStatistic -> {
+      System.out.println("Review Statistics");
+      System.out.println(reviewStatistic.getId());
+      System.out.println(reviewStatistic.getAvg());
+      System.out.println(reviewStatistic.getIsbn());
+      System.out.println(reviewStatistic.getRatings());
+      System.out.println();
+    });
+
+    assertEquals(2, result.get(0).getRatings());
+    assertEquals(2, result.get(0).getId());
+    assertEquals(new BigDecimal("3.00"), result.get(0).getAvg());
   }
 
   @Test
   void databaseShouldBeEmpty() {
+    assertEquals(0, cut.count());
   }
+
 }
